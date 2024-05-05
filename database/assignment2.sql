35e37f6ce3bad7a796b8117602d0cc8a02b1499e
reginmund9 <moronitaca@gmail.com>
2024-05-05 05:13:44 -0600

    w2-database

---
+++ database/assignment2.sql
@@ -0,0 +1,30 @@
+
+--1 Insert
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+--2 Tony Stark Update
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+--3 Tony Stark Delete
+DELETE FROM account
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+--4 Description Update
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_description LIKE '%GM Hummer%';
+
+--5 Select with Join
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory AS i
+INNER JOIN classification AS c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+
+--6 Image Update
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
