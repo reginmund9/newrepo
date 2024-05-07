096caa4a224f1734397872f4814a32514b2f1f21
reginmund9 <moronitaca@gmail.com>
2024-05-06 21:40:44 -0600

    w2-review2

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -28,3 +28,4 @@ WHERE c.classification_name = 'Sport';
 UPDATE inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
